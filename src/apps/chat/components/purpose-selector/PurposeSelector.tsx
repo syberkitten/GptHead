fdbad2398a1757b7db685c141695c22087cd0fe5
Liam Bilich <liam@cogniteam.com>
2023-07-04 17:18:19 +0300

    Added and changed icons and manifest.json for the InfiniCLI app. Adjusted tile size in PurposeSelector component.

--- src/apps/chat/components/purpose-selector/PurposeSelector.tsx
+++ src/apps/chat/components/purpose-selector/PurposeSelector.tsx
@@ -10,14 +10,13 @@ import { useUIPreferencesStore } from '~/common/state/store-ui';
 
 import { SystemPurposeId, SystemPurposes } from '../../../../data';
 import { usePurposeStore } from './store-purposes';
-import { useMediaQuery } from '@mui/material';
 
 // Constants for tile sizes / grid width - breakpoints need to be computed here to work around
 // the "flex box cannot shrink over wrapped content" issue
 //
 // Absolutely dislike this workaround, but it's the only way I found to make it work
 
-const bpTileSize = { xs: 150, md: 160, xl: 170 };
+const bpTileSize = { xs: 140, md: 150, xl: 170 };
 const tileCols = [3, 4, 6];
 const tileSpacing = 1;
 const bpMaxWidth = Object.entries(bpTileSize).reduce((acc, [key, value], index) => {
